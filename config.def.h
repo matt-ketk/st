0a764fb8572b322a23722e29553ed0c7a7c66049
Matt Ketkaroonkul <mattketk@gmail.com>
2020-10-03 20:27:38 -0700

    changed terminal font to IosevkaTerm

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Univga:pixelsize=16:antialias=true:autohint=true";
+static char *font = "IosevkaTerm:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
